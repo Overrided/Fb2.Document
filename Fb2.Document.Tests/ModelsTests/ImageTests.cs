d3fc0e86a3fd8550ee783decab852d7f6dc4e0f8
Denys Pukhkyi <den171093@gmail.com>
2023-04-16 20:48:48 +0300

    improved Image.Load tests

---
+++ Fb2.Document.Tests/ModelsTests/ImageTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Linq;
+using Fb2.Document.Constants;
+using Fb2.Document.Models;
+using FluentAssertions;
+using Xunit;
+
+namespace Fb2.Document.Tests.ModelsTests
+{
+    public class ImageTests
+    {
+        [Fact]
+        public void LoadImage_WithoutParentNode_DefaultsToInline()
+        {
+            var image = new Image();
+            image.IsInline.Should().BeTrue(); // default value
+
+            var invalidBase64Content = "someTestText";
+            var validImageXNode = new XElement(ElementNames.Image, invalidBase64Content);
+
+            image.Load(validImageXNode);
+
+            image.IsInline.Should().BeTrue();
+        }
+    }
+}
