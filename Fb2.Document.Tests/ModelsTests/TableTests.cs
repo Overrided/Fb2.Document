96fb642de13251595859307c723e0114d13b2394
Denys Pukhkyi <den171093@gmail.com>
2023-05-14 17:33:21 +0300

    adding table tests

--- Fb2.Document.Tests/ModelsTests/TableTests.cs
+++ Fb2.Document.Tests/ModelsTests/TableTests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Fb2.Document.Constants;
 using Fb2.Document.Models;
 using Fb2.Document.Models.Base;
@@ -11,6 +9,14 @@ namespace Fb2.Document.Tests.ModelsTests
 {
     public class TableTests
     {
+
+        // -------------
+        // |0.0|0.1|0.2|
+        // |---|---|---|
+        // |1.0|1.1|1.2|
+        // |---|---|---|
+        // |2.0|2.1|2.2|
+        // -------------
         [Fact]
         public void SimpleTable3x3_ToString()
         {
@@ -33,11 +39,19 @@ namespace Fb2.Document.Tests.ModelsTests
 
             var tableString = table.ToString();
             tableString
-                .Should().NotBeNullOrEmpty()
+                .Should()
+                .NotBeNullOrWhiteSpace()
                 .And
                 .Be($"-------------{Environment.NewLine}|0.0|0.1|0.2|{Environment.NewLine}|---|---|---|{Environment.NewLine}|1.0|1.1|1.2|{Environment.NewLine}|---|---|---|{Environment.NewLine}|2.0|2.1|2.2|{Environment.NewLine}-------------{Environment.NewLine}");
         }
 
+        // ---------------
+        // |0.0|  0.1.2  |
+        // |---|         |
+        // |1.0|         |
+        // |---|---------|
+        // |2.0| 2.1 |2.2|
+        // ---------------
         [Fact]
         public void Table3x3_WithColumn_AndRow_Spans_ToString()
         {
@@ -84,11 +98,19 @@ namespace Fb2.Document.Tests.ModelsTests
 
             var tableString = table.ToString();
             tableString
-                .Should().NotBeNullOrEmpty()
+                .Should()
+                .NotBeNullOrWhiteSpace()
                 .And
                 .Be($"---------------{Environment.NewLine}|0.0|  0.1.2  |{Environment.NewLine}|---|         |{Environment.NewLine}|1.0|         |{Environment.NewLine}|---|---------|{Environment.NewLine}|2.0| 2.1 |2.2|{Environment.NewLine}---------------{Environment.NewLine}");
         }
 
+        // ---------------
+        // |0.0|         |
+        // |---|         |
+        // |1.0|    0.1.2|
+        // |---|---------|
+        // |2.0| 2.1 |2.2|
+        // ---------------
         [Fact]
         public void Table3x3_WithColumn_AndRow_Spans_And_Alignments_ToString()
         {
@@ -137,10 +159,78 @@ namespace Fb2.Document.Tests.ModelsTests
 
             var tableString = table.ToString();
             tableString
-                .Should().NotBeNullOrEmpty()
+                .Should()
+                .NotBeNullOrWhiteSpace()
                 .And
                 .Be($"---------------{Environment.NewLine}|0.0|         |{Environment.NewLine}|---|         |{Environment.NewLine}|1.0|    0.1.2|{Environment.NewLine}|---|---------|{Environment.NewLine}|2.0| 2.1 |2.2|{Environment.NewLine}---------------{Environment.NewLine}");
         }
 
+        // -----------------
+        // | 0.0 |         |
+        // |-----|         |
+        // | 1.0 |    0.1.2|
+        // |---------------|
+        // |2.1.2      |2.0|
+        // |           |---|
+        // |           |3.0|
+        // -----------------
+        [Fact]
+        public void Table4x3_Multiple_Spans_And_Alignments()
+        {
+            var table = new Table();
+
+            for (int i = 0; i < 4; i++)
+            {
+                var tableRow = new TableRow();
+
+                if (i == 0) // first row with rowspan + colspan on last cell
+                {
+                    var firstCell = new TableCell();
+                    firstCell.AddTextContent($"{i}.0");
+
+                    var secondCellWithSpans = new TableCell();
+                    secondCellWithSpans.AddTextContent($"{i}.1.2");
+                    secondCellWithSpans.AddAttributes(
+                        new Fb2Attribute(AttributeNames.ColumnSpan, "2"),
+                        new Fb2Attribute(AttributeNames.RowSpan, "2"),
+                        new Fb2Attribute(AttributeNames.VerticalAlign, "Bottom"),
+                        new Fb2Attribute(AttributeNames.Align, "Right"));
+
+                    tableRow.AddContent(firstCell, secondCellWithSpans);
+                }
+
+                if (i == 1 || i == 3)
+                {
+                    var firstCell = new TableCell();
+                    firstCell.AddTextContent($"{i}.0");
+                    tableRow.AddContent(firstCell);
+                }
+
+                if (i == 2)
+                {
+                    var firstCellWithSpans = new TableCell();
+                    firstCellWithSpans.AddTextContent($"{i}.1.2");
+                    firstCellWithSpans.AddAttributes(
+                        new Fb2Attribute(AttributeNames.ColumnSpan, "2"),
+                        new Fb2Attribute(AttributeNames.RowSpan, "2"),
+                        new Fb2Attribute(AttributeNames.VerticalAlign, "Top"),
+                        new Fb2Attribute(AttributeNames.Align, "Left"));
+
+                    tableRow.AddContent(firstCellWithSpans);
+
+                    var secondCells = new TableCell();
+                    secondCells.AddTextContent($"{i}.0");
+                    tableRow.AddContent(secondCells);
+                }
+
+                table.AddContent(tableRow);
+            }
+            var tableString = table.ToString();
+            tableString
+                .Should()
+                .NotBeNullOrWhiteSpace()
+                .And
+                .Be($"-----------------{Environment.NewLine}| 0.0 |         |{Environment.NewLine}|-----|         |{Environment.NewLine}| 1.0 |    0.1.2|{Environment.NewLine}|---------------|{Environment.NewLine}|2.1.2      |2.0|{Environment.NewLine}|           |---|{Environment.NewLine}|           |3.0|{Environment.NewLine}-----------------{Environment.NewLine}");
+        }
     }
 }
