194b78082e26871ed4e71e5dd7d8e769d17cef9c
Denys Pukhkyi <den171093@gmail.com>
2023-05-14 14:47:03 +0300

    added ToString test for Table with alignments

--- Fb2.Document.Tests/ModelsTests/TableTests.cs
+++ Fb2.Document.Tests/ModelsTests/TableTests.cs
@@ -89,5 +89,58 @@ namespace Fb2.Document.Tests.ModelsTests
                 .Be($"---------------{Environment.NewLine}|0.0|  0.1.2  |{Environment.NewLine}|---|         |{Environment.NewLine}|1.0|         |{Environment.NewLine}|---|---------|{Environment.NewLine}|2.0| 2.1 |2.2|{Environment.NewLine}---------------{Environment.NewLine}");
         }
 
+        [Fact]
+        public void Table3x3_WithColumn_AndRow_Spans_And_Alignments_ToString()
+        {
+            var table = new Table();
+
+            // Generate table
+            for (int i = 0; i < 3; i++)
+            {
+                var tableRow = new TableRow();
+
+                if (i == 0) // first row with rowspan + colspan cell
+                {
+                    var firstCell = new TableCell();
+                    firstCell.AddTextContent($"{i}.0");
+
+                    var secondCellWithSpans = new TableCell();
+                    secondCellWithSpans.AddTextContent($"{i}.1.2");
+                    secondCellWithSpans.AddAttributes(
+                        new Fb2Attribute(AttributeNames.ColumnSpan, "2"),
+                        new Fb2Attribute(AttributeNames.RowSpan, "2"),
+                        new Fb2Attribute(AttributeNames.VerticalAlign, "Bottom"),
+                        new Fb2Attribute(AttributeNames.Align, "Right"));
+
+                    tableRow.AddContent(firstCell, secondCellWithSpans);
+                }
+
+                if (i == 1) // second row wit 1 cell only
+                {
+                    var firstCell = new TableCell();
+                    firstCell.AddTextContent($"{i}.0");
+                    tableRow.AddContent(firstCell);
+                }
+
+                if (i == 2) // third row with 3 cells
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        var cell = new TableCell();
+                        cell.AddTextContent($"{i}.{j}");
+                        tableRow.AddContent(cell);
+                    }
+                }
+
+                table.AddContent(tableRow);
+            }
+
+            var tableString = table.ToString();
+            tableString
+                .Should().NotBeNullOrEmpty()
+                .And
+                .Be($"---------------{Environment.NewLine}|0.0|         |{Environment.NewLine}|---|         |{Environment.NewLine}|1.0|    0.1.2|{Environment.NewLine}|---|---------|{Environment.NewLine}|2.0| 2.1 |2.2|{Environment.NewLine}---------------{Environment.NewLine}");
+        }
+
     }
 }
