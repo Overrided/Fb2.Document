c8683e3b219dabd39ad97d26460254cdf7c0b180
Denys Pukhkyi <den171093@gmail.com>
2024-06-17 23:24:22 +0300

    added sample files to Playground and Benchmark as embedded resources

--- Fb2.Document.Playground/Program.cs
+++ Fb2.Document.Playground/Program.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.IO;
-using System.Linq;
+﻿using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Fb2.Document.Constants;
 
@@ -10,40 +9,19 @@ public class Program
 {
     public async static Task Main(string[] args)
     {
-        var sampleFilePath = GetSampleFilePath();
         var fb2Document = new Fb2Document();
 
-        using (var fb2FileContentStream = new FileStream(sampleFilePath, FileMode.Open))
-        {
+        var x = Assembly.GetExecutingAssembly();
+        var names = x.GetManifestResourceNames();
+
+        // only one resource
+        using (var fb2FileContentStream = x.GetManifestResourceStream(names[0]))
             await fb2Document.LoadAsync(fb2FileContentStream);
-        }
 
         var documentString = fb2Document.ToString();
         var xmlString = fb2Document.ToXmlString();
 
         var binaryImages = fb2Document.BinaryImages;
         var firstBodyTitle = fb2Document.Bodies.FirstOrDefault()?.GetFirstChild(ElementNames.Title);
-    }
-
-    private static string GetSampleFilePath()
-    {
-        var currentFolder = Environment.CurrentDirectory;
-        var pathChunks = currentFolder.Split(Path.DirectorySeparatorChar).ToList();
-        var pathChunksTopIndex = pathChunks.Count - 1;
-        for (int i = pathChunksTopIndex; i >= 0; i--)
-        {
-            var chunk = pathChunks[i];
-            if (chunk.Equals("Fb2.Document"))
-            {
-                pathChunks.RemoveRange(i + 1, pathChunksTopIndex - i);
-                break;
-            }
-        }
-
-        pathChunks.Add("Sample");
-        pathChunks.Add("_Test_1.fb2");
-
-        var sampleFilePath = Path.Combine(pathChunks.ToArray());
-        return sampleFilePath;
     }
 }
